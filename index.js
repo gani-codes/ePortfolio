52007af31d9e7759f56b2b5fc54eb9efab9b7454
Gani <abdulgani.work@gmail.com>
2022-12-06 15:13:54 +0530

    "some fixes and changes in project"

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry);
+        // console.log(entry);
 
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
@@ -15,3 +15,19 @@ const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach(el => {
     observer.observe(el)
 });
+
+// submiting contact form data into google sheet
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxw8xwnVKof9FnLZcbuiyeuySBRcIBN0PBq1AI7PcOoaBJX-Fk0R09pjV_dhOnZLnMc/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            form.reset();
+            console.log('Success!', response)
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+
+form.reset();
