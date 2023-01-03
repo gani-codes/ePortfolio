350045a1d280518b26133ed39946374222a46662
Gani <abdulgani.work@gmail.com>
2023-01-03 17:33:49 +0530

    "added new animations using aos"

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        // console.log(entry);
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         // console.log(entry);
 
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show');
+//         } else {
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+// const hiddenElements = document.querySelectorAll('.hidden');
 
-hiddenElements.forEach(el => {
-    observer.observe(el)
-});
+// hiddenElements.forEach(el => {
+//     observer.observe(el)
+// });
 
 // submiting contact form data into google sheet
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxw8xwnVKof9FnLZcbuiyeuySBRcIBN0PBq1AI7PcOoaBJX-Fk0R09pjV_dhOnZLnMc/exec'
