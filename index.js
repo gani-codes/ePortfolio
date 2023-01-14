219f361c37023811b373803366698e17de2f713d
Gani <abdulgani.work@gmail.com>
2023-01-14 15:42:48 +0530

    "adding intersection observers to the navbar"

--- index.js
+++ index.js
@@ -16,6 +16,36 @@
 //     observer.observe(el)
 // });
 
+
+let options = {
+    rootMargin: "0px",
+    threshold: 1.0,
+};
+
+let addActive = (entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting && entry.intersectionRatio >= 0.2) {
+            let currentActive = document.querySelector("a.active-link");
+
+            if (currentActive) {
+                currentActive.classList.remove("active-link");
+            }
+
+            let newActive = document.querySelector(
+                `a[href="#${entry.target.getAttribute("id")}"]`
+            );
+            newActive.classList.add("active-link");
+        }
+    });
+};
+
+let observer = new IntersectionObserver(addActive, options);
+
+let sections = document.querySelectorAll("section");
+sections.forEach((section) => {
+    observer.observe(section);
+});
+
 // submiting contact form data into google sheet
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxw8xwnVKof9FnLZcbuiyeuySBRcIBN0PBq1AI7PcOoaBJX-Fk0R09pjV_dhOnZLnMc/exec'
 const form = document.forms['submit-to-google-sheet']
